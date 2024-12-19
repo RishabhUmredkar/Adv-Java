18b2b376905c90fe77117c8dae97d81f53462625
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-12-19 12:38:59 +0530

    Prepared Statement

--- jdbc/src/main/java/com/PrepareStatemt/Execute.java
+++ jdbc/src/main/java/com/PrepareStatemt/Execute.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Execute {
 
@@ -14,7 +15,18 @@ public class Execute {
 		String url = "jdbc:postgresql://localhost:5432/qsp?user=postgres&password=root";
 		String user = "postgres";
 		String pass = "root";
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Id");
+		int id = sc.nextInt();
+		System.out.println("Enter the name");
+		String name = sc.next();
+		System.out.println("Enter the Salary");
+		double sal = sc.nextDouble();
+		System.out.println("Enter the Phone No.");
+		long phno = sc.nextLong();
+		
+		
 			//Step 1
 			try 
 			{	
@@ -29,10 +41,10 @@ public class Execute {
 			
 				//Step 3
 				PreparedStatement ps = con.prepareStatement("insert into emp values (?,?,?,?)");
-				ps.setInt(1, 109);
-				ps.setString(2, "Rishabh");
-				ps.setLong(3, 500000);
-				ps.setLong(4, 4000);
+				ps.setInt(1, id);
+				ps.setString(2, name);
+				ps.setDouble(3, sal);
+				ps.setLong(4, phno);
 				
 				ps.execute();
 				System.out.println("done");
